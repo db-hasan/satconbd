f4cccef8c135dc9cccec66fbb1a856ab88a1fc0b
db-hasan <infoalihasanbd@gmail.com>
2024-11-17 17:10:14 +0600

    update design

--- resources/views/frontend/corporate.blade.php
+++ resources/views/frontend/corporate.blade.php
@@ -41,20 +41,16 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="bi bi-graph-up-arrow text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">IP-PABX</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Enjoy free PABX system by connecting to our IP-PABX platform</strong></span>
                                         </li>
                                         <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Zero CapEx</span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps BDIX
-                                                Bandwidth</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Scalable depending on your staffing needs</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -62,20 +58,15 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bbi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="ibi bi-globe2 text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Domain Name Registration</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Establish your business identity on the world wide web</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>IP line
-                                                service</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps BDIX
-                                                Bandwidth</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Branding</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Improve Search Engine Ranking</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -83,20 +74,14 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bbi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="bi bi-rocket-takeoff text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Web Hosting</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Hosting opportunity for your website</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>IP line
-                                                service</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps BDIX
-                                                Bandwidth</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Domain-Associated Email Address</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -104,20 +89,16 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="bi bi-hourglass-top text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Barracuda</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Shields against email-borne viruses, malware and spam</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Zero CapEx</span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Protects your network from viruses and malware.</span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps BDIX
-                                                Bandwidth</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Save Internet bandwidth</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -125,20 +106,13 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bbi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="bbi bi-gem text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Redundant Link</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>A backup link to keep your Internet operational</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>IP line
-                                                service</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps
-                                                BDIX Bandwidth</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -146,20 +120,14 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bbi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="bi bi-lightbulb text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Video Conferencing</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Online meeting with participants from remote places</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>IP line
-                                                service</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps
-                                                BDIX Bandwidth</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Demonstrate your product remotely</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -167,20 +135,16 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="i bi-bank text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Complementary Home Internet</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>One Home Internet connection for IT Manager for support from home
+                                            O</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Zero CapEx</span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>One Home Internet connection for working from home</span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps
-                                                BDIX Bandwidth</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -188,20 +152,14 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bbi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="bi bi-patch-check text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Web Application Firewall (WAF)</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Protect your websites and applications against fraud or data theft</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>IP line
-                                                service</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps
-                                                BDIX Bandwidth</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Prevents Data Leak</span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -209,20 +167,17 @@
                         <div class="col-lg-4">
                             <div class="d-flex">
                                 <div class="px-3">
-                                    <i class="bbi bi-gem text-danger" style="font-size: 40px;"></i>
+                                    <i class="bi bi-patch-check text-danger" style="font-size: 50px;"></i>
                                 </div>
                                 <div class="">
-                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Cloud IP-PABX</h3>
+                                    <h3 class="feature-heading text-dark text-start mt-2 mb-0">Data Connectivity</h3>
                                     <ul class="pt-0">
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Dedicated
-                                                    ip address</strong></span>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span><strong>Leading-edge MPLS technology for absolute privacy</strong></span>
                                         </li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>IP line
-                                                service</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Web Application
-                                                Firewall (WAF)</span></li>
-                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Unlimited Mbps
-                                                BDIX Bandwidth</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Provider independence for increased resilience</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Features service level guarantees for latency, jitter and packet loss</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>Enterprise-grade security and accessibility</span></li>
+                                        <li class="pb-0"><i class="bi bi-check text-danger"></i> <span>24/7 monitoring, management and support</span></li>
                                     </ul>
                                 </div>
                             </div>
